940e04a7f4543e7811ffd8c7e060b7be4acd0460
iaLinkrux <linkrux@gmail.com>
2025-09-20 11:12:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -194,15 +194,16 @@ window.onload = () => {
     });
 
     minimizeBtn.addEventListener('click', () => {
-        alert("¡Ouch! No me minimices, que me da complejo.");
+        alert("Una mujer minimizando a un Hombre, no es novedad");
     });
     maximizeBtn.addEventListener('click', () => {
-        alert("¡WOW! Me siento GRANDE. Aunque en realidad no cambio de tamaño.");
+        alert("Ya grande la Tengo.");
     });
     closeBtn.addEventListener('click', () => {
-        alert("No puedes cerrarme... el destino nos unió. <3");
+        alert("Y si te vas pa la pinga");
     });
 
     startLoading();
 };
+
                                          
